462ef969137ef2fbbfcb1dea4d66ed4d40009d84
Yoshiharu Habu <e205731@ie.u-ryukyu.ac.jp>
2022-12-07 19:32:48 +0900

    step3

--- src/Report5.java
+++ src/Report5.java
@@ -1,20 +1,7 @@
-
-
-
 public class Report5 {
     public static void main(String[] args){
-        String str = null;
-        try{
-            if(str != null) {
-                System.out.println(str.length());
-            }else{
-                throw new NullPointerException();
-            }
-        }catch(NullPointerException e){
-            System.out.println("NullPointerException Error が発生しました。");
-            System.out.println(e.getMessage());
-
-        }
+        String str = "壱百万";
+        int value = Integer.parseInt(str);
     }
 
 }
